b7bcc38edfdf35766ce65f16a2d25ef32f95d13a
dmarnol2 <dmarnol2@asu.edu>
2019-02-20 23:51:56 -0500

    begin making app dynamic starting with ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,32 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    
+        // props is an object not an array so we cant loop through
+        // must transform object to array so we can loop and get values
+        // Object below is a JS object not a react object
+        //change const below to let since we are going to change value of transformedIngredients further below
+        let transformedIngredients = Object.keys(props.ingredients)
+        //chain a method and we can use map since keys returns an array
+        // map executes a fn on each element in the input array
+            .map(ingredientKey => {
+                return [...Array(props.ingredients[ingredientKey])].map((_,index)=>{
+                    return <BurgerIngredient key={ingredientKey+index} type={ingredientKey}/>;
+                });
+            }) //returns array of keys ie {salad,meat,bacon,cheese}
+            .reduce((array,element)=>{
+                return array.concat(element)
+            },[]);
+
+        console.log(transformedIngredients);
+        if(transformedIngredients.length===0){
+            transformedIngredients=<p>Please start adding ingredients</p>
+        }
+
         return(
          
             <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
             </div>
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,11 +4,27 @@ import Burger from '../../components/Burger/Burger'
 import Aux from '../../higherOrderComp/Aux';
 
 class BurgerBuilder extends Component{
+    // 1 way to initialize state but we will use following way
+    // constructor (props) = {
+    //     super(props)
+    //     this.state={...}
+
+    // }
+
+   state ={
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    }
+    }
+
     render(){  // important lifecycle method telling react what you want to displsy
         return(
             // need wrapping component because we are returning 2 adjacent components
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls component</div>
             </Aux>
 
