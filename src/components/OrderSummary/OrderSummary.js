364545d3a43cc0e44466e82af5b7938461b49c7f
dmarnol2 <dmarnol2@asu.edu>
2019-03-15 17:41:27 -0400

    add Lifecycle hooks to orderSummary and Modal

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../HOC/Aux';
 import Button from '../UI/Button/Button';
 
-const orderSummary = (props) => {
-    // const ingredientsSummary = props.ingredients; // this is an object want an array to use below
-    const ingredientsSummary = Object.keys(props.ingredients)
+//convert to class so we can add lifecycle hooks to prevent unnecessary rerendering
+// const orderSummary = (props) => {
+    class OrderSummary extends Component {
+
+        componentWillUpdate(){
+            console.log("orderSummary updated")
+        };
+        render(){
+            const ingredientsSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             // double curly brace is used for JS, cap 1st letter of ingredient
             // return list of igKey which is ingredient and props.ingredients[igKey] which is value of the igKey
             return (<li key={igKey}>
-                    <span style={{textTransform:'capitalize'}}>{igKey}</span>:{props.ingredients[igKey]}
+                    <span style={{textTransform:'capitalize'}}>{igKey}</span>:{this.props.ingredients[igKey]}
                     </li>
                     )
             })
-    // need wrapping div since we have multiple elements and we already
-    // have one in AUX
-    return(
+            return(
     <Aux>
         <h3>Your Order</h3>
         <p>A burger with the following ingredients:</p>
@@ -23,17 +27,51 @@ const orderSummary = (props) => {
             {/* using default css styling of Aux */}
             {ingredientsSummary}
         </ul>
-        <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
         <p>Continue to checkout?</p>
         <Button
             buttonType={'Danger'} 
-            clicked={props.purchaseCancelled}>CANCEL</Button>
+            clicked={this.props.purchaseCancelled}>CANCEL</Button>
         <Button
             buttonType={'Success'}
-            clicked={props.purchaseContinued}>CONTINUE</Button>
+            clicked={this.props.purchaseContinued}>CONTINUE</Button>
     </Aux>
-    );
+            );
+    }
+}
+    
+export default OrderSummary;
 
-};
+//     }=(props)=>{
+//     // const ingredientsSummary = props.ingredients; // this is an object want an array to use below
+//     const ingredientsSummary = Object.keys(props.ingredients)
+//         .map(igKey => {
+//             // double curly brace is used for JS, cap 1st letter of ingredient
+//             // return list of igKey which is ingredient and props.ingredients[igKey] which is value of the igKey
+//             return (<li key={igKey}>
+//                     <span style={{textTransform:'capitalize'}}>{igKey}</span>:{props.ingredients[igKey]}
+//                     </li>
+//                     )
+//             })
+//     // need wrapping div since we have multiple elements and we already
+//     // have one in AUX
+//     return(
+//     <Aux>
+//         <h3>Your Order</h3>
+//         <p>A burger with the following ingredients:</p>
+//         <ul>
+//             {/* using default css styling of Aux */}
+//             {ingredientsSummary}
+//         </ul>
+//         <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+//         <p>Continue to checkout?</p>
+//         <Button
+//             buttonType={'Danger'} 
+//             clicked={props.purchaseCancelled}>CANCEL</Button>
+//         <Button
+//             buttonType={'Success'}
+//             clicked={props.purchaseContinued}>CONTINUE</Button>
+//     </Aux>
+//     );
 
-export default orderSummary;
+// };
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
 import Aux from '../../../HOC/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
+// convert to class
+class Modal extends Component{
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
+    shouldComponentUpdate(nextProps, nextState){
+        console.log("in should comp update of MODAL");
+        //below is the same as just returning the check
+        // if(nextProps.show !== this.props.show){
+        //     return true;
+        // }
+        return nextProps.show !== this.props.show;
+    };
+
+    componentWillUpdate(){
+        console.log("in compwillUpdate of MODAL");
+    }
+
+    render(){
+        return(
+
+            <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
     <div className={classes.Modal}
     style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1':'0'
+        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: this.props.show ? '1':'0'
     }}>
-        {props.children}
+        {this.props.children}
     </div>
     </Aux>
-);
-export default modal;
+
+        );
+    }
+
+}
+export default Modal;
